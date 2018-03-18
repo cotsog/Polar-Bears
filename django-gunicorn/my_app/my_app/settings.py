a0ae83b7b7a80fd99fcc12873858458f25bb3e90
global2alex <global2alex@gmail.com>
2018-03-18 10:11:23 -0400

    Added env_file and variable

--- django-gunicorn/my_app/my_app/settings.py
+++ django-gunicorn/my_app/my_app/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2*6i(v_u4%p-l1&4-lxzn$8w9r5honbqwvl&$kgyuvrhrhee!b'
+# SECRET_KEY = os.getenv['DJANGO__SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
